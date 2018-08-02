3449088b6643a1f733a293cb3ae25bd15d8d6f10
Shane <loppys9@gmail.com>
2018-08-01 22:03:10 -0400

    Basic character template.

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ import mss.tools
 import numpy
 import pytesseract
 
+import characters
+
 # Things to keep track of
 """
 boss_health
@@ -17,6 +19,8 @@ current_character_stats
 current_character_abilities
 boss_ability_cooldowns
 """
+
+MATCH_VALUE = 10000
 
 def get_swgoh_box(sct):
     monitor = sct.monitors[1]
@@ -36,14 +40,14 @@ def get_phase(img):
     p3_blue = 10
     p4_green = 40
 
-    print(img.shape)
+    #print(img.shape)
     x = 1195
     blue = img.item(15, x, 0)
     green = img.item(15, x, 1)
     red = img.item(15, x, 2)
 
     print(red, green, blue)
-    if green > 180:
+    if green > 150:
         return 1
     elif green > 100:
         return 2
@@ -69,7 +73,29 @@ def get_health(img):
     health = pytesseract.image_to_string(health_section, lang='eng', config=config)
     return float(health.replace(' ', '.')[:-1])
 
+def get_character(chars, img):
+    y = img.shape[0]
+    x = img.shape[1]
+
+    start_y = int(y * 0.77)
+    start_x = int(x * 0.035)
+    end_y = start_y + int(y * 0.074)
+    end_x = start_x + int(x * .045)
+    ch_section = img[start_y:end_y, start_x:end_x]
+
+    cv2.imwrite('res_troop.png', ch_section)
+    #cv2.imshow('health', ch_section)
+    #cv2.waitKey()
+
+    # Might want to return the min result value as this can error.
+    for c in chars:
+        res = cv2.matchTemplate(ch_section, c.img, cv2.TM_SQDIFF)
+        if res[0][0] < MATCH_VALUE:
+            return c
+
+
 if __name__ == '__main__':
+    chars = characters.read_from_config('characters.json')
     with mss.mss() as sct:
         swbox = get_swgoh_box(sct)
         sct_img = sct.grab(swbox)
@@ -81,4 +107,6 @@ if __name__ == '__main__':
         img = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
         starting_health = get_health(img)
         print(starting_health)
+        cur_char = get_character(chars, img)
+        print(cur_char)
 
--- characters.py
+++ characters.py
@@ -1,36 +1,77 @@
+import json
+import numpy
+import cv2
+
 class Buffs:
     #Buffs are a tuple of % chance and number of turns.
-    self.call_to_action = (0, 0)
-    self.daze = (0, 0)
-    self.deathmark = (0, 0)
-    self.foresight = (0, 0)
-    self.fracture = (0, 0)
-    self.isolate = (0, 0)
-    self.speed_down = (0, 0)
-    self.speed_up = (0, 0)
-    self.stealth = (0, 0)
-    self.stun = (0, 0)
-    self.tenacity = (0, 0)
-    self.tm_reduce = (0, 0)
+
+    def __init__(self, buffs):
+        #print(buffs)
+        self.advantage = (0, 0)
+        self.call_to_action = (0, 0)
+        self.daze = (0, 0)
+        self.deathmark = (0, 0)
+        self.foresight = (0, 0)
+        self.fracture = (0, 0)
+        self.isolate = (0, 0)
+        self.speed_down = (0, 0)
+        self.speed_up = (0, 0)
+        self.stealth = (0, 0)
+        self.stun = (0, 0)
+        self.tenacity = (0, 0)
+        self.tm_reduce = (0, 0)
+        self.tm_gain = (0, 0)
+
+        for b in buffs:
+            for key, value in b.items():
+                self.__dict__[key] = (value[0], value[1])
+
+class Abilities:
+
+    def __init__(self):
+        self.allies = False
+        self.selfs = False
+        self.leader = False
+
+        self.accuracy = 0
+        self.crit_chance = 0
+        self.crit_dmg = 0
+        self.protection = 0
+        self.health = 0
+        self.potency = 0
 
 class Attack:
-    self.targets_ally = False
-    self.affect_allies = False
 
-    def __init__(self, basic, cooldown=0):
-        self.basic = basic
-        self.target_buffs = Buffs()
-        self.ally_buffs = Buffs()
-        self.target_ally_buffs = Buffs()
-        self.cooldown = 0
+    def __init__(self, spec):
+        self.name = spec['name']
+        self.basic = spec['basic']
+        self.targets_ally = spec['targets_ally']
+        self.affect_allies = spec['affects_ally']
+        self.aoe = spec['aoe']
+        self.cooldown = spec['cooldown']
+        self.target_buffs = Buffs(spec['target_buffs'])
+        self.ally_buffs = Buffs(spec['ally_buffs'])
+        self.self_buffs = Buffs(spec['self_buffs'])
+        self.target_ally_buffs = Buffs(spec['target_ally_buffs'])
 
 
 class Character:
-    self.attacks = []
-    self.abilities = []
 
-    def __init__(self, speed):
-        self.speed = speed
+    # Should probably have a serializer here but meh.
+    def __init__(self, specs):
+        self.name = specs['name']
+        self.speed = specs['speed']
+        self.factions = specs['factions']
+
+        self.attacks = []
+        for attack in specs['attacks']:
+            self.attacks.append(Attack(attack))
+
+        self.get_img(specs['img'])
+
+
+    def get_img(self, img_file):
+        self.img = cv2.imread('chars/' + img_file, 0)
 
     def gives_foresight():
         pass
@@ -41,4 +82,13 @@ class Character:
     def removes_tm():
         pass
 
-r2d2 = Character(220)
+def read_from_config(config_file):
+    with open(config_file) as json_file:
+        data = json.load(json_file)
+
+    characters = []
+    for ch in data:
+        #print(ch)
+        characters.append(Character(ch))
+
+    return characters
